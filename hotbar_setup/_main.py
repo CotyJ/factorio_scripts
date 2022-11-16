b35a93e8c9349867ed0109e200fa3828056ecdb2
Coty Janeway <coty.janeway@gmail.com>
2022-11-16 04:20:52 -0700

    Created final hotbar coordinates data with algorith

--- hotbar_setup/_main.py
+++ hotbar_setup/_main.py
@@ -6,11 +6,24 @@ import pandas as pd
 
 # Item boxes are 40x40 including spacing
 item_box = 40
-# print("Starting Mouse Mapping")
+gap = 4
 
+# Starting position on my laptop screen
+starting_position = [776,  1048]
+
+
+# Import Coordinates file
 coordinates = pd.read_csv('coordinates.csv')
 print(coordinates)
 
+# Moves mouse over all hotkey positions
+for key in coordinates:
+  mouse.move(*coordinates[key])
+  print(key, *coordinates[key])
+  time.sleep(.05)
+
+
+# Watches the mouse and prints coordinates
 def map_mouse():
   time.sleep(.5)
 
@@ -45,7 +58,6 @@ def map_mouse():
 
 # map_mouse()
 
-time.sleep(.5)
 
 # Starting position, starting with 4-1
 # mouse.move(776,  1048)
@@ -73,16 +85,34 @@ time.sleep(.5)
 
 
 # build the dictionary for coordinates.py
+
 rows = 10
 columns = 4
-starting_coords = [776,  1048]
-mouse.move(*starting_coords)
+#
+# time.sleep(.3)
+# starting_coords1 = [776,  1048]
+# mouse.move(*starting_coords1)
+#
+# time.sleep(.3)
+# starting_coords2 = [776,  1008]
+# mouse.move(*starting_coords2)
+#
+# time.sleep(.3)
+# starting_coords3 = [776,  968]
+# mouse.move(*starting_coords3)
+#
+# time.sleep(.3)
+# starting_coords4 = [776,  928]
+# mouse.move(*starting_coords4)
+
+
+# Dataframe to export
 coords = create_coordinates(rows, columns)
 
 coords_df = pd.DataFrame(coords)
 
 coords_df.to_csv('coordinates.csv')
 
-print(coords)
-print('')
-print(coords_df)
+# print(coords)
+# print('')
+# print(coords_df)
--- hotbar_setup/create_coordinates.py
+++ hotbar_setup/create_coordinates.py
@@ -9,6 +9,7 @@ def create_coordinates(row, col):
 
   row_list = []
   col_list = []
+
   while rows_start <= row:
     row_list.append(rows_start)
     rows_start += 1
@@ -20,7 +21,26 @@ def create_coordinates(row, col):
   for row in row_list:
     for col in col_list:
       newstr1 = str(row)
-      newstr2 = str(col)
-      newstr = newstr1 + "-" + newstr2
-      dic[newstr] = [0, 0]
+      # newstr2 = str(col)
+      newstr = str(row) + "-" + str(col)
+
+      # here
+      starting_position = [776, 1048]
+      print("col ", int(newstr1))
+      # if first half (left 5)
+      if int(newstr1) <= 5:
+        print("less than equals 5")
+        dic[newstr] = [starting_position[0] + (40 * (row - 1)), starting_position[1] - (40 * (col - 1))]
+      else:
+        print("greater than 5")
+        dic[newstr] = [starting_position[0] + (40 * (row - 1)) + 4, starting_position[1] - (40 * (col - 1))]
+
+
+      # if second half (right 5)
+
+  # How to calculate AND insert the correct values?
+  # if there is one row, then 1-1 is  [776,  1048]
+  # if there are four,   then 4-1 is  [776,  1048]
+  # and offsets the rest, now 1-1 is  [776,  928]
+  print(dic)
   return dic

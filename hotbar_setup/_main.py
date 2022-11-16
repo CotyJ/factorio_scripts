d54f0c735ed121b8d5d21f5f412cab8442b9fbf2
Coty Janeway <coty.janeway@gmail.com>
2022-11-16 02:10:16 -0700

    Create algorithm to create coordinates dictionary

--- hotbar_setup/_main.py
+++ hotbar_setup/_main.py
@@ -2,10 +2,11 @@ import mouse
 import time
 from coordinates import coordinates
 from items import items
+from create_coordinates import create_coordinates
 
 # Item boxes are 40x40 including spacing
 item_box = 40
-print("Starting Mouse Mapping")
+# print("Starting Mouse Mapping")
 
 
 def map_mouse():
@@ -27,5 +28,57 @@ def map_mouse():
   print(pos)
   time.sleep(.5)
 
+  pos = mouse.get_position()
+  print(pos)
+  time.sleep(.5)
 
-map_mouse()
+  pos = mouse.get_position()
+  print(pos)
+  time.sleep(.5)
+
+  pos = mouse.get_position()
+  print(pos)
+  time.sleep(.5)
+
+
+# map_mouse()
+
+time.sleep(.5)
+
+# Starting position, starting with 4-1
+# mouse.move(776,  1048)
+# time.sleep(.5)
+# mouse.move(816,  1048)
+# time.sleep(.5)
+# mouse.move(856,  1048)
+# time.sleep(.5)
+# mouse.move(896,  1048)
+# time.sleep(.5)
+# mouse.move(936,  1048)
+# time.sleep(.5)
+
+# 10 pixel gap between left and right half
+# mouse.move(980,  1048)
+# time.sleep(.5)
+# mouse.move(1020, 1048)
+# time.sleep(.5)
+# mouse.move(1060, 1048)
+# time.sleep(.5)
+# mouse.move(1100, 1048)
+# time.sleep(.5)
+# mouse.move(1140, 1048)
+# time.sleep(.5)
+
+
+# build the dictionary for coordinates.py
+rows = 10
+columns = 4
+starting_coords = [776,  1048]
+mouse.move(*starting_coords)
+coords = create_coordinates(rows, columns)
+
+
+# print(ls)
+
+
+print(coords)
---
+++ hotbar_setup/create_coordinates.py
@@ -0,0 +1,22 @@
+def create_coordinates(row, col):
+  rows_start = 1
+  columns_start = 1
+  dic = {}
+
+  row_list = []
+  col_list = []
+  while rows_start <= row:
+    row_list.append(rows_start)
+    rows_start += 1
+
+  while columns_start <= col:
+    col_list.append(columns_start)
+    columns_start += 1
+
+  for row in row_list:
+    for col in col_list:
+      newstr1 = str(row)
+      newstr2 = str(col)
+      newstr = newstr1 + "-" + newstr2
+      dic[newstr] = [0, 0]
+  return dic

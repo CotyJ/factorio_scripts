40591599ead165bf409ef4c83eb130d446c25254
Coty Janeway <coty.janeway@gmail.com>
2022-11-16 04:56:52 -0700

    Rename and restructure again for readability

--- hotbar_setup/_main.py
+++ hotbar_setup/_main.py
@@ -7,63 +7,24 @@ import pandas as pd
 
 # Item boxes are 40x40 including spacing
 item_box = 40
-gap = 4
 
 # Starting position on my laptop screen
 starting_position = [776,  1048]
 
 
 # Import Coordinates file
-coordinates = pd.read_csv('coordinates.csv')
+hotbar_coordinates = pd.read_csv('hotbar_coordinates.csv')
 # print(coordinates)
 
 # Moves mouse over all hotkey positions
-for key in coordinates:
-  mouse.move(*coordinates[key])
-
+for key in hotbar_coordinates:
+  mouse.move(*hotbar_coordinates[key])
   # Reset buttons
   # mouse.click(button='middle')
   # keyboard.press_and_release('escape')
   # keyboard.press_and_release('escape')
-
-  print(key, *coordinates[key])
+  print(key, *hotbar_coordinates[key])
   time.sleep(.02)
-
-
-# Watches the mouse and prints coordinates
-def map_mouse():
-  time.sleep(.5)
-
-  pos = mouse.get_position()
-  print(pos)
-  time.sleep(.5)
-
-  pos = mouse.get_position()
-  print(pos)
-  time.sleep(.5)
-
-  pos = mouse.get_position()
-  print(pos)
-  time.sleep(.5)
-
-  pos = mouse.get_position()
-  print(pos)
-  time.sleep(.5)
-
-  pos = mouse.get_position()
-  print(pos)
-  time.sleep(.5)
-
-  pos = mouse.get_position()
-  print(pos)
-  time.sleep(.5)
-
-  pos = mouse.get_position()
-  print(pos)
-  time.sleep(.5)
-
-
-# map_mouse()
 
 
 # build the dictionary for coordinates.py
@@ -73,4 +34,4 @@ columns = 4
 
 # Dataframe to export as CSV
 # coords_df = pd.DataFrame(coords)
-# coords_df.to_csv('coordinates.csv')
+# coords_df.to_csv('hotbar_coordinates.csv')
---
+++ hotbar_setup/mouse_finder.py
@@ -0,0 +1,34 @@
+# Watches the mouse and prints coordinates
+def map_mouse():
+  time.sleep(.5)
+
+  pos = mouse.get_position()
+  print(pos)
+  time.sleep(.5)
+
+  pos = mouse.get_position()
+  print(pos)
+  time.sleep(.5)
+
+  pos = mouse.get_position()
+  print(pos)
+  time.sleep(.5)
+
+  pos = mouse.get_position()
+  print(pos)
+  time.sleep(.5)
+
+  pos = mouse.get_position()
+  print(pos)
+  time.sleep(.5)
+
+  pos = mouse.get_position()
+  print(pos)
+  time.sleep(.5)
+
+  pos = mouse.get_position()
+  print(pos)
+  time.sleep(.5)
+
+
+map_mouse()
